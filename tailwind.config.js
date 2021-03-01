1d7e8b93f03794d5fd2d4c56d4a2ba962fa8ccbb
unknown <nikhilkutinha+github@pm.me>
2021-03-01 06:50:12 +0530

    Fixed purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
     "./index.html",
-    ".public/index.html",
+    "./public/index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   darkMode: false, // or 'media' or 'class'
